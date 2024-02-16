52280ca21dd3d4009b2fd7f249d88501f44c99c9
Fyodor Shklyar <30theflame05@gmail.com>
2024-02-16 12:21:05 +0300

    SETTINGS: setup environment

---
+++ .eslintrc.cjs
@@ -0,0 +1,68 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  ignorePatterns: ["build", ".eslintrc.cjs"],
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "react",
+    "react-refresh",
+    "simple-import-sort",
+    "import",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".ts", ".tsx"],
+      },
+      typescript: {},
+    },
+  },
+  rules: {
+    "react/function-component-definition": [2, { namedComponents: "arrow-function" }],
+    "react/react-in-jsx-scope": 0,
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [
+          ["^react", "^@?\\w"],
+          ["^(@|components)(/.*|$)"],
+          ["^\\u0000"],
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+        ],
+      },
+    ],
+  },
+};
